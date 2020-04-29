a1d86200a80bf64f16e93531e40e925e7e736351
Getlar <tamastheactual@gmail.com>
2020-04-29 19:11:35 +0200

    Add project files.

---
+++ WebAPI_Client/DataProviders/PersonDataProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI_Client.Models;
+
+namespace WebAPI_Client.DataProviders
+{
+    public static class PersonDataProvider
+    {
+        private static string url = "http://localhost:5000/api/person";
+
+        public static IList<Person> GetPeople()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(url).Result;
+            };
+        }
+    }
+}
